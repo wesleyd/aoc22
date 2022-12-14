e78acb9f5a7e7ac34d35ef9414e70f46fa3065c2
Wesley Darlington <wesley.darlington@gmail.com>
2022-12-14 11:07:02 -0500

    day2

--- 02.py
+++ 02.py
@@ -13,8 +13,8 @@ assert parse(example) == [['A', 'Y'], ['B', 'X'], ['C', 'Z']]
 
 # chr(ord('X') - ord('X') + ord('A'))
 
-def score(moves):
-    them, me = moves
+def score(pair):
+    them, me = pair
     # Rock (A) is 1, Paper (B) is 2, Scissors (C) is 3.
     p = ord(me) - ord('X') + 1
     # 0 for a loss, 3 for a draw, 6 for a win
@@ -39,5 +39,36 @@ def part1(input):
 assert part1(example) == 15
 
 with open('day02.txt') as f:
-    print("Day02 part 1 => ", part1(f.read()))
+    print("Day02 part 1 => ", part1(f.read()))  # => 15632
 
+guide = {
+    ('A', 'X'): 'C',
+    ('B', 'X'): 'A',
+    ('C', 'X'): 'B',
+    ('A', 'Y'): 'A',
+    ('B', 'Y'): 'B',
+    ('C', 'Y'): 'C',
+    ('A', 'Z'): 'B',
+    ('B', 'Z'): 'C',
+    ('C', 'Z'): 'A',
+}
+
+def score2(pair):
+    them, goal = pair
+    me = guide[(them, goal)]
+    # Rock (A) is 1, Paper (B) is 2, Scissors (C) is 3.
+    p = ord(me) - ord('A') + 1
+    q = (ord(goal) - ord('X')) * 3
+    return p+q
+
+assert score2(['A', 'Y']) == 4
+assert score2(['B', 'X']) == 1
+assert score2(['C', 'Z']) == 7
+
+def part2(input):
+    return sum(map(score2, parse(input)))
+
+assert part2(example) == 12
+
+with open('day02.txt') as f:
+    print("Day02 part 2 => ", part2(f.read()))  # => 14416
